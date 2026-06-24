6bde5a545105c002a846872526db279561a0264e
coman-paul-31461 <paul.coman1995@gmail.com>
2026-06-24 12:09:44 +0300

    Update app.js 5

--- app.js
+++ app.js
@@ -175,7 +175,7 @@ app.get('/idm-qrlogin/:env/:region/', (req, res) => {
     return res.status(404).json({ error: `No URL configured for env=${env} region=${region}` });
   }
 
-  return res.send(targetUrl);
+  return res.json({ google_qrcode: targetUrl });
 });
 
 
